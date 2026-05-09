70fb5db7fe24dd7212753dc2a866944f7606e8d2
Raman Hassan <ramanhasan1997@hotmail.com>
2026-05-09 16:28:06 +0200

    feat(googleFit): Track estimated steps
    Include the `estimated_steps` data source for step count tracking to ensure more accurate data is pulled from Google Fit.

--- src/lib/googleFit.ts
+++ src/lib/googleFit.ts
@@ -13,6 +13,7 @@ export async function fetchGoogleFitData(accessToken: string) {
       },
       {
         dataTypeName: 'com.google.step_count.delta',
+        dataSourceId: 'derived:com.google.step_count.delta:com.google.android.gms:estimated_steps'
       },
       {
         dataTypeName: 'com.google.heart_minutes',
